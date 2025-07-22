60b5cb6eab98efb98977fec48d25fecc52879090
Shivam6388469930 <prasadshivam930@gmail.com>
2025-07-22 12:20:55 +0530

    change logo

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -279,16 +279,13 @@ const Header: React.FC<HeaderProps> = ({ transparent = false }) => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
         {/* Logo & Nav */}
         <div className="flex items-center">
-          <Link href="/">
-            <Image
-              className={transparent ? '' : 'dark:invert'}
-              src="/next.svg"
-              alt="Logo"
-              width={120}
-              height={30}
-              priority
-            />
-          </Link>
+            <Link href="/" className="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clipRule="evenodd" />
+                <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
+              </svg>
+              <span className="text-xl font-bold">Sarthak Consultancy</span>
+            </Link>
 
           {/* Desktop Nav */}
           <nav className="ml-10 space-x-4 lg:space-x-8 hidden md:flex">
